493beeb81a4fd03fbafef6631a2d69f9aca0fb0c
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-09 17:28:07 +0200

    javascript completo/ refactoring css_html

--- jsgrid.js
+++ jsgrid.js
@@ -3,3 +3,52 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+//selezion elementi nel DOM
+const elementInfo = document.querySelector('.info');
+const elementGrid = document.querySelector ('.grid');
+const buttonPlay = document.querySelector('#play');
+
+// Istruzioni bottone
+buttonPlay.addEventListener('click', function(){
+
+    //Nascondo messaggio iniziale
+    elementInfo.classList.add('hidden');
+
+    //Appare contorno griglia
+    elementGrid.classList.remove('hidden');
+
+    //Numero celle 
+    const n_Cells = 100;
+
+    //Genero griglia completa
+    createGrid(n_Cells, elementGrid);
+});
+
+
+
+// FUNZIONI
+function createGrid (n_Cells, elementContainer){
+    console.log(n_Cells);
+
+
+    //Reset schermata griglia 
+    elementContainer.innerHtml = '';
+
+    for (let i = 1; i <= n_Cells; i++){
+        //Creazione cella con stile css
+        const elementCell = document.createElement('div');
+        elementCell.innerHTML = i;
+        elementCell.classList.add('cell');
+        
+
+        elementContainer.append(elementCell);
+        
+        //Aggiunta azione alla cella creata
+        elementCell.addEventListener('click', function(){
+            console.log(this);
+            console.log('Selezionata cella N.' + this.innerHTML);
+            this.classList.add('clicked');
+        });
+    }
+}
